88c664c987414ceef8bfd3959e0d4f5ecc10a5c1
volodymyr1787 <vovaposta@gmail.com>
2022-12-14 01:08:29 +0200

    update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,10 +1,22 @@
-body.style.backgroundColor = getRandomHexColor();
+const button = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+ 
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+const newColor = getRandomHexColor();
+
+button.addEventListener('click', () => {
+  // const backgroundColor = evt.currentTarget.value;
+  document.body.style.backgroundColor = getRandomHexColor();
+  span.textContent = document.body.style.backgroundColor;
+});
 
 
 
 
 
+// body.style.backgroundColor = getRandomHexColor();
