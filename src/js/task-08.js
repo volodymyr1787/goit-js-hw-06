2987c604015cdb6a3120ba6b19abfdf67c4c2dc6
volodymyr1787 <vovaposta@gmail.com>
2022-12-13 23:47:58 +0200

    update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    const formElements = evt.currentTarget.elements;
+
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+    const formData = {
+        mail,
+        password
+    };
+
+    console.log (formData);
+
+    evt.currentTarget.reset()
+
+};
+
 
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,10 @@
+body.style.backgroundColor = getRandomHexColor();
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+
+
+
